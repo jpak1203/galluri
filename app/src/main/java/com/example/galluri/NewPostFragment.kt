ff2eb77d499acb8a33b85c7f62b9bd8770a2816b
Justin Pak <jpak1203@gmail.com>
2019-08-07 21:21:22 -0400

    select picture from Gallery to switch ImageButtonView

--- app/src/main/java/com/example/galluri/NewPostFragment.kt
+++ app/src/main/java/com/example/galluri/NewPostFragment.kt
@@ -6,12 +6,25 @@ import android.util.Log
 import android.view.*
 import android.widget.EditText
 import android.view.KeyEvent.KEYCODE_ENTER
-
-
+import android.content.Intent
+import android.support.v4.view.accessibility.AccessibilityEventCompat.setAction
+import android.widget.Button
+import android.widget.ImageButton
+import android.app.Activity
+import android.graphics.Bitmap
+import android.R.attr.data
+import android.support.v4.app.NotificationCompat.getExtras
+import android.content.Intent.getIntent
+import android.net.Uri
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
+import android.provider.MediaStore
+import android.widget.ImageView
 
 
 class NewPostFragment : Fragment() {
 
+    val PICK_IMAGE = 1
     //todo: Database Implementation
     //todo: Upload picture and update placeholder
     //todo: Fix text-box appearance when creating title and description
@@ -21,42 +34,72 @@ class NewPostFragment : Fragment() {
         val rootView = inflater.inflate(R.layout.fragment_new_post, container, false)
 
         val editText = rootView.findViewById<EditText>(R.id.description_text)
-        editText.setOnKeyListener(object : View.OnKeyListener {
+        editText.setOnKeyListener { v, keyCode, event ->
+            // if enter is pressed start calculating
+            if (keyCode == KEYCODE_ENTER && event.action === KeyEvent.ACTION_UP) {
 
-            override fun onKey(v: View, keyCode: Int, event: KeyEvent): Boolean {
+                // get EditText text
+                val text = (v as EditText).text.toString()
 
-                // if enter is pressed start calculating
-                if (keyCode == KEYCODE_ENTER && event.getAction() === KeyEvent.ACTION_UP) {
+                // find how many rows it contains
+                val editTextRowCount = text.split("\\n".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray().size
 
-                    // get EditText text
-                    val text = (v as EditText).text.toString()
+                Log.d("Row", editTextRowCount.toString())
+                // user has input more than limited - lets do something
+                // about that
+                if (editTextRowCount >= 25) {
 
-                    // find how many rows it contains
-                    val editTextRowCount = text.split("\\n".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray().size
+                    // find the last break
+                    val lastBreakIndex = text.lastIndexOf("\n")
 
-                    Log.d("Row", editTextRowCount.toString())
-                    // user has input more than limited - lets do something
-                    // about that
-                    if (editTextRowCount >= 25) {
+                    // compose new text
+                    val newText = text.substring(0, lastBreakIndex)
 
-                        // find the last break
-                        val lastBreakIndex = text.lastIndexOf("\n")
+                    // add new text - delete old one and append new one
+                    // (append because I want the cursor to be at the end)
+                    v.setText("")
+                    v.append(newText)
 
-                        // compose new text
-                        val newText = text.substring(0, lastBreakIndex)
-
-                        // add new text - delete old one and append new one
-                        // (append because I want the cursor to be at the end)
-                        v.setText("")
-                        v.append(newText)
-
-                    }
                 }
-
-                return false
             }
-        })
+
+            false
+        }
+
+        val addImage = rootView.findViewById<ImageButton>(R.id.add_image)
+
+        addImage.setOnClickListener {
+            val intent = Intent()
+            intent.setType("image/*")
+            intent.setAction(Intent.ACTION_GET_CONTENT)
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE)
+        }
+
         return rootView
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE) {
+            if (resultCode == Activity.RESULT_OK) {
+                val uri = data?.data
+                val bitmap = MediaStore.Images.Media.getBitmap(this.context?.contentResolver, uri)
+                val ob = BitmapDrawable(this.resources, bitmap)
+
+                val addImage = view?.findViewById<ImageButton>(R.id.add_image)
+                addImage?.background = ob
+                addImage?.adjustViewBounds = true
+                addImage?.scaleType = ImageView.ScaleType.CENTER_CROP
+                addImage?.setImageResource(android.R.color.transparent)
+
+                Log.d("Result", addImage.toString())
+                Log.d("Result", uri.toString())
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+                //Write your code if there's no result
+            }
+        }
     }
 
     companion object {
